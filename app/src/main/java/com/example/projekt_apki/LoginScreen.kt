507240982406b866cbcceee9e1f250ab44324b48
mateusz-sobierajski <mateusz.sobierajski.official@gmail.com>
2022-12-13 16:30:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projekt_apki/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.projekt_apki
+
+import android.os.Bundle
+
+class LoginScreen {
+
+    }
+}
---
+++ app/src/main/java/com/example/projekt_apki/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projekt_apki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
+    }
+}
