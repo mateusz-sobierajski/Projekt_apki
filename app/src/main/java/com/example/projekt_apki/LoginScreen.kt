2aabdcf514b1d88e0720e2b9499337b971ba3c38
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2022-12-15 01:39:06 +0100

    Dodanie settingow i panelu logowania/rejestracji, ujednolicenie nazw

--- app/src/main/java/com/example/projekt_apki/LoginScreen.kt
+++ app/src/main/java/com/example/projekt_apki/LoginScreen.kt
@@ -1,8 +1,27 @@
 package com.example.projekt_apki
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
-class LoginScreen {
+class LoginScreen : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        val loginButton = this.findViewById<AppCompatButton>(R.id.LoginButton)
+
+        loginButton.setOnClickListener { logIn() }
+        //registrationButton.setOnClickListener(View.OnClickListener { openRegistrationPage() })
+    }
+
+    private fun openRegistrationPage() {
+    }
+
+    private fun logIn() {
+        val intent = Intent(this, Settings::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/projekt_apki/Settings.kt
@@ -0,0 +1,22 @@
+package com.example.projekt_apki
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
+class Settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val logoutButton = this.findViewById<AppCompatButton>(R.id.LogoutButton)
+        logoutButton.setOnClickListener(View.OnClickListener { logout() })
+    }
+
+    private fun logout() {
+        val intent = Intent(this, LoginScreen::class.java)
+        startActivity(intent)
+    }
+}
